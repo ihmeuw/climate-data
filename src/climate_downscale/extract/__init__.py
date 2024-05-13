b3d663a9e6a1ae8e411f9471a2cd77e07ba75d0c
collijk <collijk@uw.edu>
2024-05-13 12:42:22 -0700

    Local climate zone extraction

--- src/climate_downscale/extract/__init__.py
+++ src/climate_downscale/extract/__init__.py
@@ -7,7 +7,6 @@ from climate_downscale.extract.ncei_climate_stations import (
 )
 from climate_downscale.extract.rub_local_climate_zones import (
     extract_rub_local_climate_zones,
-    extract_rub_local_climate_zones_task,
 )
 from climate_downscale.extract.srtm_elevation import (
     extract_srtm_elevation,
@@ -24,6 +23,6 @@ RUNNERS = {
 TASK_RUNNERS = {
     "ncei": extract_ncei_climate_stations,
     "era5": extract_era5_task,
-    "lcz": extract_rub_local_climate_zones_task,
+    "lcz": extract_rub_local_climate_zones,
     "elevation": extract_srtm_elevation_task,
 }
--- src/climate_downscale/extract/rub_local_climate_zones.py
+++ src/climate_downscale/extract/rub_local_climate_zones.py
@@ -1,13 +1,27 @@
+from pathlib import Path
+
 import click
+from rra_tools.shell_tools import wget
+
+from climate_downscale.data import ClimateDownscaleData
+
+URL_TEMPLATE = "https://zenodo.org/records/8419340/files/{file_name}?download=1"
+FILES = [
+    "00_global_map_of_LCZs.png",
+    "lcz_filter_v3.tif",
+    "lcz_probability_v3.tif",
+    "lcz_v3.tif",
+    "readme.txt",
+]
 
 
-def extract_rub_local_climate_zones_main() -> None:
-    pass
+def extract_rub_local_climate_zones_main(output_dir: str | Path) -> None:
+    data = ClimateDownscaleData(output_dir)
+    out_root = data.rub_local_climate_zones
 
-
-@click.command()
-def extract_rub_local_climate_zones_task() -> None:
-    raise NotImplementedError
+    for file_name in FILES:
+        url = URL_TEMPLATE.format(file_name=file_name)
+        wget(url, out_root / file_name)
 
 
 @click.command()
