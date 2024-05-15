4017162b963b3c6838c99e8113919f554cb7817e
James Collins <collijk@uw.edu>
2024-05-14 21:17:48 -0700

    Fix permissions, run all years

--- src/climate_downscale/extract/ncei_climate_stations.py
+++ src/climate_downscale/extract/ncei_climate_stations.py
@@ -5,11 +5,11 @@ from pathlib import Path
 import click
 import pandas as pd
 from rra_tools.cli_tools import with_output_directory
-from rra_tools.shell_tools import wget, mkdir
+from rra_tools.shell_tools import wget, mkdir, touch
 
 from climate_downscale.data import DEFAULT_ROOT, ClimateDownscaleData
 
-EXTRACTION_YEARS = list(range(1990, 1992))
+EXTRACTION_YEARS = list(range(1990, 2024))
 URL_TEMPLATE = (
     "https://www.ncei.noaa.gov/data/global-summary-of-the-day/archive/{year}.tar.gz"
 )
@@ -37,7 +37,9 @@ def extract_ncei_climate_stations_main(output_dir: str | Path) -> None:
         print('concatting')
         data = pd.concat(dfs)
         print('writing')
-        data.to_parquet(cd_data.ncei_climate_stations / "climate_stations.parquet")
+        out_path = cd_data.ncei_climate_stations / "climate_stations.parquet"
+        touch(out_path)
+        data.to_parquet(out_path)
     finally:
         shutil.rmtree(download_dir)
 
