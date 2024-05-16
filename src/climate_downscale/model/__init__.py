4b43fb79e4bd90cc3a0932912d3b8e7f3ac08d37
collijk <collijk@uw.edu>
2024-05-16 13:49:53 -0700

    Add prep training data

--- src/climate_downscale/model/__init__.py
+++ src/climate_downscale/model/__init__.py
@@ -2,11 +2,17 @@ from climate_downscale.model.prepare_predictors import (
     prepare_predictors,
     prepare_predictors_task,
 )
+from climate_downscale.model.prepare_training_data import (
+    prepare_training_data,
+    prepare_training_data_task,
+)
 
 RUNNERS = {
     "prepare_predictors": prepare_predictors,
+    "prepare_training_data": prepare_training_data,
 }
 
 TASK_RUNNERS = {
     "prepare_predictors": prepare_predictors_task,
+    "prepare_training_data": prepare_training_data_task,
 }
